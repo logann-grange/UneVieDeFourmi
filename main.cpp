de40ab96f83e058466ce4ae36bbb07b26eab5f94
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-05-22 08:59:22 +0200

    rajout d'un menu de choix d'algo en attente du reste

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Fourmiliere.hpp"
 #include <iostream>
 #include "ResolutionBFS.hpp"
+using namespace std;
 
 int main(int argc, char* argv[]) {
     Fourmiliere f;
@@ -10,9 +11,39 @@ int main(int argc, char* argv[]) {
     }
     if (f.chargerDepuisFichier("formilliere/" + std::string(argv[1]))) {
         f.afficher();
-        ResolutionBFS(f);
     } else {
         std::cerr << "Erreur lors du chargement du fichier." << std::endl;
+    }
+
+    cout << "==============================\n";
+    cout << "      MENU DE RÉSOLUTION      " << endl;
+    cout << "==============================\n";
+    cout << "Veuillez choisir un algorithme :\n";
+    cout << "  1. Résolution par BFS" << endl;
+    cout << "  2. Résolution par DFS" << endl;
+    cout << "  3. Combinaison BFS/DFS" << endl;
+    cout << "------------------------------" << endl;
+    cout << "Votre choix : ";
+    int choix;
+    cin >> choix;
+    cout << "------------------------------" << endl;
+    switch (choix) {
+        case 1:
+            cout << "\n[INFO] Résolution par BFS sélectionnée.\n";
+            ResolutionBFS(f);
+            break;
+        case 2:
+            cout << "\n[INFO] Résolution par DFS sélectionnée.\n";
+            // f.resoudreDFS();
+            cout << "DFS non implémenté." << endl;
+            break;
+        case 3:
+            cout << "\n[INFO] Résolution par combinaison BFS/DFS sélectionnée.\n";
+            // f.resoudreCombinaison();
+            cout << "Combinaison non implémentée." << endl;
+            break;
+        default:
+            cout << "\n[ERREUR] Choix invalide. Veuillez relancer le programme et choisir une option valide." << endl;
     }
     return 0;
 }
